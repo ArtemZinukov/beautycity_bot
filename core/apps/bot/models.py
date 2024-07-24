d2721022fd3965164622b27cfe25ff08b04a1890
Misha <m.mazaixin@yandex.ru>
2024-07-24 23:32:59 +0300

    Update models.py

--- core/apps/bot/models.py
+++ core/apps/bot/models.py
@@ -10,3 +10,30 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+
+class Client(models.Model):
+    username = models.CharField(max_length=50)
+
+
+class Service(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=2, decimal_places=2)
+
+
+class Master(models.Model):
+    name = models.CharField(max_length=50)
+    service = models.ForeignKey(Service)
+
+
+class Salon(models.Model):
+    adress = models.CharField(max_length=100)
+    master = models.ForeignKey(Master)
+
+
+class Registration(models.Model):
+    salon = models.ForeignKey(Salon)
+    master = models.ForeignKey(Master)
+    client = models.ForeignKey(Client)
+    service = models.ForeignKey(Service)
+    time_registration = models.DateTimeField(auto_now=False, auto_now_add=False)
